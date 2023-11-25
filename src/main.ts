7e25fe3a06246fefebaf6d3f08ce00de93cb65da
Aki Kanellis <hello@akikanellis.com>
2023-11-25 21:37:50 +0000

    feat: validate branch name

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
+
+function retrieveBranchName(): string {
+  const ref = github.context.ref
+  const branchRefPrefix = 'refs/heads/'
+
+  if (ref.startsWith(branchRefPrefix)) {
+    return ref.substring(branchRefPrefix.length)
+  } else {
+    throw new Error('Could not determine the branch name')
+  }
+}
 
 /**
  * The main function for the action.
@@ -7,18 +18,25 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const eventName = github.context.eventName
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds...`)
+    if (eventName != 'push') {
+      throw Error(`Event '${eventName}' is not supported`)
+    }
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const payload = github.context.payload
+    core.info(`Validating branch name for event '${eventName}' with payload '${payload}'`)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const regex = RegExp(core.getInput('regex'))
+    const branchName = retrieveBranchName()
+
+    const testResult = regex.test(branchName)
+
+    if (!testResult) {
+      core.setFailed(`Branch ${branchName} failed regex validation '${regex}'`)
+    } else {
+      core.info("Branch validation succeeded!")
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
