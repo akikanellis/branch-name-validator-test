01cb608184955b52565ea9cb9429e1f3ffa697a6
Aki Kanellis <hello@akikanellis.com>
2023-12-02 20:17:05 +0000

    build: set tsconfig.json

---
+++ test/main.test.ts
@@ -0,0 +1,18 @@
+/**
+ * Unit tests for the action's main functionality, src/main.ts
+ *
+ * These should be run as if the action was called from a workflow.
+ * Specifically, the inputs listed in `action.yml` should be set as environment
+ * variables following the pattern `INPUT_<INPUT_NAME>`.
+ */
+
+// Mock the GitHub Actions core library
+describe("action", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("sets the time output", async () => {
+    expect(true);
+  });
+});
