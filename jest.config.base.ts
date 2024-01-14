c8c87d913e9e37157f28ab6253b8016b94070b11
Aki Kanellis <hello@akikanellis.com>
2024-01-14 20:27:55 +0000

    nest project

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -1,11 +1,8 @@
 import type { Config } from "jest";
-import nxPreset from "@nx/jest/preset";
 
 export default {
-  ...nxPreset,
   collectCoverageFrom: ["src/**/*.ts"],
   coverageReporters: ["json", "html", "text"],
-  displayName: "branch-name-validator-test",
   resetMocks: true,
   testEnvironment: "node",
   testMatch: ["**/test/**/*.test.ts"],
---
+++ packages/branch-name-validator-test/jest.config.ts
@@ -0,0 +1,5 @@
+import type { Config } from "jest";
+
+export default {
+  ...require("../../jest.config.base"),
+} as Config;
