61d1576778300c8b869a8e0cf1fc02d0a86aabdb
Aki Kanellis <hello@akikanellis.com>
2024-01-15 23:45:11 +0000

    build: fix coverage

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -1,12 +1,12 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  collectCoverageFrom: ["src/**/*.ts"],
+  collectCoverageFrom: ["**/src/**/*.ts"],
   coverageReporters: ["json", "html", "text"],
   resetMocks: true,
   testEnvironment: "node",
   testMatch: ["**/test/**/*.test.ts"],
-  transform: { "^.+\\.ts$": "ts-jest" },
+  transform: { "^.+\\.tsx?$": "ts-jest" },
   verbose: true,
 };
 
